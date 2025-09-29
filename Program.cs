d7460bb4fe385f2a3404b9949ea4e86b31e59c35
Hlodvik <lctiv@outlook.com>
2025-09-29 12:42:15 -0600

    8

--- Program.cs
+++ Program.cs
@@ -5,24 +5,27 @@ using Vend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Db + Identity (yours as-is)
+// Db + Identity
 builder.Services.AddDbContext<IdContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         sql => sql.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)));
-builder.Services
-    .AddDefaultIdentity<AppUser>(o =>
-    {
-        o.SignIn.RequireConfirmedAccount = false; // until email is wired up
-        o.Password.RequireDigit = true;
-        o.Password.RequireUppercase = true;
-        o.Password.RequiredLength = 8;
-    })
-    .AddEntityFrameworkStores<IdContext>();
 
-// Keep Razor Pages for Identity UI
+builder.Services.AddDefaultIdentity<AppUser>(o =>
+{
+    o.SignIn.RequireConfirmedAccount = false; // until email is wired up
+    o.Password.RequireDigit = true;
+    o.Password.RequireUppercase = true;
+    o.Password.RequiredLength = 8;
+})
+.AddEntityFrameworkStores<IdContext>();
+ 
+builder.Services.AddAuthentication()
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
+ 
 builder.Services.AddRazorPages();
 
-// This is the Blazor Web App (server interactivity)
+// Blazor Web App (server interactivity)
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -36,15 +39,15 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseRouting();
+ 
+app.UseAntiforgery();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 // Identity UI pages
 app.MapRazorPages();
-
-// Mount your component app (replaces _Host.cshtml approach)
+ 
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode();
 
